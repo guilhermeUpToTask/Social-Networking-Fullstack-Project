1d4f4bb8d8cd859c873ac3879517ab765aa84928
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-02-27 05:08:05 -0300

    chore[backend]: secret_key env created and implemented in config.py file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -23,7 +23,10 @@ class Settings(BaseSettings):
     DATABASE_HOST: str
     DATABASE_NAME: str
     DATABASE_PORT: int
-
+    SECRET_KEY: str
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    
     @computed_field  # type: ignore[prop-decorator]
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
