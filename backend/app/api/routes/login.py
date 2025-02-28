d8148c567d3b7cb873450cc786961de85a911f24
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-02-28 05:19:30 -0300

    feat[auth]: authentication deps created

---
+++ backend/app/api/routes/login.py
@@ -0,0 +1,8 @@
+from datetime import timedelta
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app import crud
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -40,3 +40,15 @@ class UsersPublic(SQLModel):
 
 class Message (SQLModel):
     message: str
+    
+class TokenPayload(SQLModel):
+    sub: str | None = None
+    
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
+    
